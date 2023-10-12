7dde954b50603b8def0bc83827507b02b4c81c5e
Vitor <you@example.com>
2023-10-12 02:01:42 -0300

    feat: Realizado conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const logger = require('./utils/logging');
+const figlet = require('figlet');
+const routes = require('./routers/routes');
+const connectDB = require('./database/connection')
 
 const app = express();
-const logger = require('./utils/logging');
-
 const PORT = process.env.PORT || 8080;
 
+connectDB()
 
 app.use(logger);
+app.use('/api', routes); //Prefixo de todos os endpoints
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
+    console.log(figlet.textSync("ops-monitoring",{horizontalLayout: 'full'}));   
     console.log(`Servidor rodando na porta ${PORT}`);
 });
 
