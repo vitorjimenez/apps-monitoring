758a19adbf671d42fb47bc9ed450939806594a10
Kevin Gomes <kevinmg50@gmail.com>
2023-10-15 23:05:57 -0300

    feat: implementado helmet como middleware de segurança

---
+++ middlewares/helmet.js
@@ -0,0 +1,19 @@
+const helmet = require('helmet');
+
+const secureHeaders = () => {
+    return helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                styleSrc: ["'self'", "'unsafe-inline'"], 
+                mediaSrc: ["'self'"],
+                frameSrc: ["'none'"], 
+            },
+        },
+        dnsPrefetchControl: false,
+        frameguard: { action: 'deny' },
+        hidePoweredBy: true,
+    });
+};
+
+module.exports = secureHeaders;
