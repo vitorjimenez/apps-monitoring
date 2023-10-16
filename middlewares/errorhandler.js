44f3c35ac3bfc9d84de166a1f3f2c0860fe7686a
Kevin Gomes <kevinmg50@gmail.com>
2023-10-16 12:24:56 -0300

    feat: middleware de errorhandler com disparo de alarmes

---
+++ middlewares/errorhandler.js
@@ -0,0 +1,13 @@
+const { sendDiscordNotification } = require('../services/webhooks');
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+    console.error(`Erro ocorrido em ${req.method} ${req.url} : ${err.message}`);
+    err.method = req.method;
+    err.url = req.url;
+
+    sendDiscordNotification(err);
+
+    res.status(500).send({ error: 'Ocorreu um erro inesperado!' });
+};
+
+module.exports = errorHandlerMiddleware;
