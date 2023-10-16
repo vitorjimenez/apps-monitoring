4dce7ee541a20a99a341517d837b7cc007847383
Kevin Gomes <kevinmg50@gmail.com>
2023-10-16 12:05:25 -0300

    feat: rota tempóraria para simulação de erros e envio de webhooks

--- routers/routes.js
+++ routers/routes.js
@@ -10,10 +10,25 @@ const router = express.Router();
  *       200:
  *         description: Retorna a mensagem 'api-up'
  */
-router.get('/', (req, res) => {
+router.get('/diag', (req, res) => {
     res.json({
         message: 'api-up'
     });
+});
+
+/**
+ * @swagger
+ * /error:
+ *   get:
+ *     summary: Simula um erro para testar o webhook
+ *     responses:
+ *       500:
+ *         description: Simula um erro inesperado
+ */
+router.get('/error', (req, res, next) => {
+    // Simulando um erro
+    const simulatedError = new Error('Este é um erro simulado para testar o webhook!');
+    next(simulatedError);
 });
 
 module.exports = router;
